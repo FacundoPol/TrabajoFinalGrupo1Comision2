775bc137d4ba7836395d6542faf52518a0d7fe3f
Ezequiel Villalba <ezequielvillagf@gmail.com>
2025-06-09 20:22:15 -0300

    Modifico app.jsx para usar AppRoutes - Ezequiel Villalba

--- tp_final_pv_grupo1/src/App.jsx
+++ tp_final_pv_grupo1/src/App.jsx
@@ -1,11 +1,15 @@
+// src/App.jsx
+import { BrowserRouter } from "react-router-dom";
+import AppNavbar from "./components/Navbar";
+import AppRoutes from "./AppRoutes";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <AppNavbar />
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
