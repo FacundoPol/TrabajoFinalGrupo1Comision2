c5d8805501b2310e400358f048f65f7b9d27c32e
Gerardo <leon.capo2000@gmail.com>
2025-06-08 22:10:40 -0300

    se agregaron los componentes Header,Navbar,Footer reutilizables para la  navegacion del sitio -AramayoGerardo

---
+++ tp_final_pv_grupo1/src/assets/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer(){
+    return (
+        <footer>
+            <div>
+                <p>© todos los derechos reservados - Grupo 1 - Programacion Visual </p>
+                <p>Integrantes: Abregu Miguel, Aramayo Gerardo, Pol Facundo, Rios Sofia,
+                    Villalba Ezequiel
+                </p>
+            </div>
+            
+        </footer>
+    );
+}
