851368bfb4dea8720efbf5a8e5c3e3265bc140d6
Ezequiel Villalba <ezequielvillagf@gmail.com>
2025-06-09 20:36:24 -0300

    Correcion de importacion en AppRoutes - Ezequiel Villalba

--- tp_final_pv_grupo1/src/assets/routes/AppRoutes.jsx
+++ tp_final_pv_grupo1/src/assets/routes/AppRoutes.jsx
@@ -2,14 +2,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Favoritos from "../pages/Favoritos";
-import Acerca from "../pages/About";
+import About from "../pages/About";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/favoritos" element={<Favoritos />} />
-      <Route path="/acerca-de" element={<Acerca />} />
+      <Route path="/acerca-de" element={<About />} />
     </Routes>
   );
 }
