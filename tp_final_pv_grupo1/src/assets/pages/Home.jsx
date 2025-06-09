7b0221386c946a0eddcc7f8ab45be3573c6ff483
Miguel Angel Abregu <miguel_af_555@hotmail.com>
2025-06-09 03:21:48 -0300

    creacion de Pages y estructuracion - Home ,Favoritos , About - Miguel Abregu

---
+++ tp_final_pv_grupo1/src/assets/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { Container } from 'react-bootstrap'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+export default Acerca(){
+
+    return(
+      < Container>
+      <Header></Header>
+      <h1>
+      Lista de Productos
+      </h1>
+     <Footer></Footer>
+      </Container> 
+
+    )
+}
