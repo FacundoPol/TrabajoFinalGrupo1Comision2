aa6f6cd5ea5949d65a2ae2ac79e335a020865fbc
Gerardo <leon.capo2000@gmail.com>
2025-06-16 04:24:05 -0300

    se agrego el componente ListaFavoritos.jsx para filtrar los productos favoritos seleccionado y se importa a Favoritos.jsx para mostrar los productos seleccionados

---
+++ tp_final_pv_grupo1/src/assets/components/ListaFavoritos.jsx
@@ -0,0 +1,50 @@
+import { useProductos } from '../hooks/useProductos';
+import { Container, Row, Col, Card, Button, CardBody } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom'
+
+//exporto el componente ListaFavoritos para usarlo dentro de Favoritos.jsx
+//y mostrar los productos marcados como fav. 
+export default function ListaFavoritos(){
+    // llamo a los arrays productos,favoritos y la funcion toogleFavorito declarados 
+    // en ProductosContexts.jsx mediante el hook useProductos()
+    const {productos, favoritos, toggleFavorito} = useProductos()
+    
+    /*creo un nuevo array llamado productosFavoritos que contiene sola los productos
+    cuyo ID esta presente en el array favoritos*/ 
+    const productosFavoritos = productos.filter((p) => favoritos.includes(p.id))
+
+    return (
+        <Container>
+            <Row>
+                {productosFavoritos.lenght === 0 ? (
+                 <p>No hay productos marcados como favoritos</p>   
+                ):(
+                    productosFavoritos.map((producto) => (
+                        <Col key={producto.id} className='mb-4'>
+                            <Card>
+                                <Card.Img
+                                    variant="top"
+                                    src={producto.image}
+                                    style={{
+                                        height: '200px',
+                                        objectFit: 'contain',
+                                        backgroundColor: '#f8f9fa'
+                                    }}
+                                    />
+                                    <CardBody>
+                                        <Card.Title>{producto.title}</Card.Title>
+                                        <Card.Text>${producto.price}</Card.Text>
+                                        <Button
+                                            variant="danger"
+                                            onClick={() => toggleFavorito(producto.id)}> Quitar de Favoritos
+                                        </Button>
+                                    </CardBody>
+                            </Card>
+                        </Col>
+                    )
+                ))
+            }
+            </Row>
+        </Container>
+    )
+} 
